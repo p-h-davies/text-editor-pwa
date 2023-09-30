1864a3c0ee6c90221d6dfcfb83ca939c7d416a1a
Philippa Davies <philippadavies603@gmail.com>
2023-09-30 15:55:11 +1000

    wepback installation complete, cache completed, db & install started

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'J.A.T.E',
+        short_name: 'JATE',
+        description: 'A basic text editor',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        start_url: "/",
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96],
+            destination: path.join('assets', 'icons')
+          }],
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+      ],
+    },
+  };
+};
